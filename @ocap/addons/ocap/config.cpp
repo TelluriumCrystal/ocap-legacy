8e2ff7be1de6b786819841c36eecfa14515cb6e3
Andrew Bennett <admin@andrewbennet1.com>
2020-07-05 00:34:18 -0400

    Added initial module config

--- @ocap/addons/ocap/config.cpp
+++ @ocap/addons/ocap/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
 		name = "OCAP";
 		author = "MisterGoodson";
 		requiredAddons[] = {"A3_Functions_F", "cba_main"};
-		units[] = {};
+		units[] = {"OCAP_ModuleInit"};
 		weapons[] = {};
 	};
 };
@@ -17,7 +17,7 @@ class CfgFunctions
 		class null
 		{
 			file = "ocap\functions";
-			class init {postInit = 1;};
+			class init {};
 			class exportData {};
 			class callExtension {};
 			class startCaptureLoop {};
@@ -28,6 +28,106 @@ class CfgFunctions
 			class eh_hit {};
 			class eh_disconnected {};
 			class eh_connected {};
+		};
+	};
+};
+
+class CfgVehicles
+{
+	class Logic;
+	class Module_F: Logic
+	{
+		class AttributesBase
+		{
+			class Default;
+			class Edit;					// Default edit box (i.e., text input field)
+			class Combo;				// Default combo box (i.e., drop-down menu)
+			class Checkbox;				// Default checkbox (returned value is Bool)
+			class CheckboxNumber;		// Default checkbox (returned value is Number)
+			class ModuleDescription;	// Module description
+			class Units;				// Selection of units on which the module is applied
+		};
+		// Description base classes, for more information see below
+		class ModuleDescription
+		{
+			class EmptyDetector;
+		};
+	};
+	class OCAP_ModuleInit: Module_F
+	{
+		// Standard object definitions
+		scope = 2; // Editor visibility; 2 will show it in the menu, 1 will hide it.
+		displayName = "OCAP Recorder"; // Name displayed in the menu
+		category = "NO_CATEGORY";
+
+		// Name of function triggered once conditions are met
+		function = "ocap_fn_init";
+		// Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
+		functionPriority = 1;
+		// 0 for server only execution, 1 for global execution, 2 for persistent global execution
+		isGlobal = 0;
+		// 1 for module waiting until all synced triggers are activated
+		isTriggerActivated = 1;
+		// 1 if modules is to be disabled once it's activated (i.e., repeated trigger activation won't work)
+		isDisposable = 1;
+		// 1 to run init function in Eden Editor as well
+		is3DEN = 0;
+
+		// Menu displayed when the module is placed or double-clicked on by Zeus
+		curatorInfoType = "RscDisplayAttributeModuleNuke";
+
+		// Module attributes, uses https://community.bistudio.com/wiki/Eden_Editor:_Configuring_Attributes#Entity_Specific
+		class Attributes: AttributesBase
+		{
+			// Arguments shared by specific module type (have to be mentioned in order to be present)
+			class Units: Units
+			{
+				property = "ocap_ModuleInit_Units";
+			};
+			// Module specific arguments
+			class Yield: Combo
+			{
+				// Unique property, use "<moduleClass>_<attributeClass>" format to make sure the name is unique in the world
+				property = "myTag_ModuleNuke_Yield";
+				displayName = "Nuclear weapon yield"; // Argument label
+				tooltip = "How strong will the explosion be"; // Tooltip description
+				typeName = "NUMBER"; // Value type, can be "NUMBER", "STRING" or "BOOL"
+				defaultValue = "50"; // Default attribute value. WARNING: This is an expression, and its returned value will be used (50 in this case)
+				class Values
+				{
+					class 50Mt	{name = "50 megatons";	value = 50;}; // Listbox item
+					class 100Mt	{name = "100 megatons"; value = 100;};
+				};
+			};
+			class Name: Edit
+			{
+				displayName = "Name";
+				tooltip = "Name of the nuclear device";
+				// Default text filled in the input box
+				// Because it's an expression, to return a String one must have a string within a string
+				defaultValue = """Tsar Bomba""";
+			};
+			class ModuleDescription: ModuleDescription{}; // Module description should be shown last
+		};
+
+		// Module description. Must inherit from base class, otherwise pre-defined entities won't be available
+		class ModuleDescription: ModuleDescription
+		{
+			description = "Short module description"; // Short description, will be formatted as structured text
+			sync[] = {"LocationArea_F"}; // Array of synced entities (can contain base classes)
+
+			class LocationArea_F
+			{
+				description[] = { // Multi-line descriptions are supported
+					"First line",
+					"Second line"
+				};
+				position = 0; // Position is taken into effect
+				direction = 0; // Direction is taken into effect
+				optional = 1; // Synced entity is optional
+				duplicate = 1; // Multiple entities of this type can be synced
+				synced[] = {"EmptyDetector"}; // Pre-define entities like "AnyBrain" can be used. See the list below
+			};
 		};
 	};
 };
--- @ocap/addons/ocap/functions/fn_init.sqf
+++ @ocap/addons/ocap/functions/fn_init.sqf
@@ -6,68 +6,79 @@
 	Capture loop is automatically started once init complete.
 */
 
-// Define global vars
-#include "\userconfig\ocap\config.hpp";
-ocap_entitiesData = [];  // Data on all units + vehicles that appear throughout the mission.
-ocap_eventsData = []; // Data on all events (involving 2+ units) that occur throughout the mission.
-ocap_captureFrameNo = 0; // Frame number for current capture
-ocap_endFrameNo = 0; // Frame number at end of mission
+// Get Module inputs
+_logic = param [0,objNull,[objNull]];
+_units = param [1,[],[[]]];
+_activated = param [2,true,[true]];
 
-// Add mission EHs
-addMissionEventHandler ["EntityKilled", {
-	_victim = _this select 0;
-	_killer = _this select 1;
+if(_activated) then {
 
-	// Check entity is initiliased with OCAP
-	// TODO: Set ocap_exclude to true if unit is not going to respawn (e.g. AI)
-	if (_victim getVariable ["ocap_isInitialised", false]) then {
-		[_victim, _killer] call ocap_fnc_eh_killed;
+	// Define global vars
+	#include "\userconfig\ocap\config.hpp";
+	ocap_entitiesData = [];  // Data on all units + vehicles that appear throughout the mission.
+	ocap_eventsData = []; // Data on all events (involving 2+ units) that occur throughout the mission.
+	ocap_captureFrameNo = 0; // Frame number for current capture
+	ocap_endFrameNo = 0; // Frame number at end of mission
 
-		{
-			_victim removeEventHandler _x;
-		} forEach (_victim getVariable "ocap_eventHandlers");
-	};
-}];
+	// Add mission EHs
+	addMissionEventHandler ["EntityKilled", {
+		_victim = _this select 0;
+		_killer = _this select 1;
 
-// Transfer ID from old unit to new unit
-// Mark old body to now be excluded from capture
-addMissionEventHandler ["EntityRespawned", {
-	_newEntity = _this select 0;
-	_oldEntity = _this select 1;
+		// Check entity is initiliased with OCAP
+		// TODO: Set ocap_exclude to true if unit is not going to respawn (e.g. AI)
+		if (_victim getVariable ["ocap_isInitialised", false]) then {
+			[_victim, _killer] call ocap_fnc_eh_killed;
 
-	if (_oldEntity getVariable ["ocap_isInitialised", false]) then {
-		_newEntity setVariable ["ocap_isInitialised", true];
-		_id = _oldEntity getVariable "ocap_id";
-		_newEntity setVariable ["ocap_id", _id];
-		_newEntity setVariable ["ocap_exclude", false];
-		_oldEntity setVariable ["ocap_exclude", true]; // Exclude old body from capture
-
-		_newEntity call ocap_fnc_addEventHandlers;
-	};
-}];
-
-addMissionEventHandler["HandleDisconnect", {
-	_unit = _this select 0;
-	_name = _this select 3;
-
-	if (_unit getVariable ["ocap_isInitialised", false]) then {
-		_unit setVariable ["ocap_exclude", true];
-	};
-
-	_name call ocap_fnc_eh_disconnected;
-}];
-
-addMissionEventHandler["PlayerConnected", {
-	_name = _this select 2;
-
-	_name call ocap_fnc_eh_connected;
-}];
-
-if (ocap_endCaptureOnEndMission) then {
-	addMissionEventHandler ["Ended", {
-		["Mission ended."] call ocap_fnc_log;
-		[] call ocap_fnc_exportData;
+			{
+				_victim removeEventHandler _x;
+			} forEach (_victim getVariable "ocap_eventHandlers");
+		};
 	}];
+
+	// Transfer ID from old unit to new unit
+	// Mark old body to now be excluded from capture
+	addMissionEventHandler ["EntityRespawned", {
+		_newEntity = _this select 0;
+		_oldEntity = _this select 1;
+
+		if (_oldEntity getVariable ["ocap_isInitialised", false]) then {
+			_newEntity setVariable ["ocap_isInitialised", true];
+			_id = _oldEntity getVariable "ocap_id";
+			_newEntity setVariable ["ocap_id", _id];
+			_newEntity setVariable ["ocap_exclude", false];
+			_oldEntity setVariable ["ocap_exclude", true]; // Exclude old body from capture
+
+			_newEntity call ocap_fnc_addEventHandlers;
+		};
+	}];
+
+	addMissionEventHandler["HandleDisconnect", {
+		_unit = _this select 0;
+		_name = _this select 3;
+
+		if (_unit getVariable ["ocap_isInitialised", false]) then {
+			_unit setVariable ["ocap_exclude", true];
+		};
+
+		_name call ocap_fnc_eh_disconnected;
+	}];
+
+	addMissionEventHandler["PlayerConnected", {
+		_name = _this select 2;
+
+		_name call ocap_fnc_eh_connected;
+	}];
+
+	if (ocap_endCaptureOnEndMission) then {
+		addMissionEventHandler ["Ended", {
+			["Mission ended."] call ocap_fnc_log;
+			[] call ocap_fnc_exportData;
+		}];
+	};
+
+	[] spawn ocap_fnc_startCaptureLoop;
 };
 
-[] spawn ocap_fnc_startCaptureLoop;
+// Dummy function return
+true
