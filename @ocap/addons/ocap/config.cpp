590dd54c7597a412c479e1610a14243d6963365f
Andrew Bennett <admin@andrewbennet1.com>
2020-07-11 12:25:05 -0400

    Added export module

--- @ocap/addons/ocap/config.cpp
+++ @ocap/addons/ocap/config.cpp
@@ -72,7 +72,7 @@ class CfgVehicles
 	{
 		// Standard object definitions
 		scope = 2; 						// Editor visibility; 2 will show it in the menu, 1 will hide it.
-		displayName = "OCAP Recorder"; 	// Name displayed in the menu
+		displayName = "OCAP Init"; 		// Name displayed in the menu
 		category = "ocap_category";		// Editor category ("NO_CATEGORY = "Misc")
 		function = "ocap_fnc_init";		// Name of function triggered once conditions are met
 		functionPriority = 1;			// Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
@@ -124,6 +124,21 @@ class CfgVehicles
 				typeName = "BOOL";
 				defaultValue = "false";
 			};
+		};
+	};
+	
+	class OCAP_ModuleExport: Module_F
+	{
+		// Standard object definitions
+		scope = 2; 						// Editor visibility; 2 will show it in the menu, 1 will hide it.
+		displayName = "OCAP Export"; 	// Name displayed in the menu
+		category = "ocap_category";		// Editor category ("NO_CATEGORY = "Misc")
+		function = "ocap_fnc_init";		// Name of function triggered once conditions are met
+		functionPriority = 1;			// Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
+		isGlobal = 0;					// 0 for server only execution, 1 for global execution, 2 for persistent global execution
+		isTriggerActivated = 1;			// 1 for module waiting until all synced triggers are activated
+		isDisposable = 0;				// 1 if modules is to be disabled once it's activated (i.e., repeated trigger activation won't work)
+		is3DEN = 0;						// 1 to run init function in Eden Editor as well
 		};
 	};
 };
