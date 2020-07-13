c6c1211cb7d944ec047cdbb07ada8a86417b39f3
Andrew Bennett <admin@andrewbennet1.com>
2020-07-13 07:36:58 -0400

    Reworked to editor modules (#3)
    * Added initial module config
    
    * Added eden editor module
    
    * Rewrote fn_init to be called by module
    
    * Added mission JSONs to .gitignore
    
    * Improved fn_init parameters and added comments
    
    * Removed unused module code
    
    * Added CBA settings setup
    
    * Changed CBA setting to act as a default if module value is not set
    
    * Cleaned up module init
    
    * Added export module
    
    * Fixed incorrect function for export module
    
    * Removed extra semicolin
    
    I'm kinda amazed this worked before this fix
    
    * Removed userconfig file
    
    * Removed module from units list
    
    * Improved comments on config

--- @ocap/addons/ocap/config.cpp
+++ @ocap/addons/ocap/config.cpp
@@ -1,8 +1,8 @@
+// Mod meta info
 class CfgPatches
 {
 	class OCAP
 	{
-		// Editor meta info
 		name = "OCAP";
 		author = "OCAP Team";
 		url = "https://github.com/TelluriumCrystal/ocap-revived";
@@ -13,6 +13,7 @@ class CfgPatches
 	};
 };
 
+// All functions to be loaded into the game
 class CfgFunctions
 {
 	class OCAP
@@ -20,17 +21,126 @@ class CfgFunctions
 		class null
 		{
 			file = "ocap\functions";
-			class init {postInit = 1;};
-			class exportData {};
-			class callExtension {};
-			class startCaptureLoop {};
-			class addEventHandlers {};
-			class log {};
-			class eh_killed {};
-			class eh_fired {};
-			class eh_hit {};
-			class eh_disconnected {};
-			class eh_connected {};
+			class init {};				// Initializes OCAP and starts recording
+			class init_CBASettings{};	// Sets up the custom CBA settings option
+			class exportData {};		// Stops recording and starts exporting data
+			class callExtension {};		// Calls the OCAP exporter .dll
+			class startCaptureLoop {};	// Starts the capture loop, which sustains itself afterwards
+			class addEventHandlers {};	// Adds event handlers to a specific unit
+			class log {};				// Writes a string to the screen and/or the .rpt file
+			class eh_killed {};			// Event handler for unit killed
+			class eh_fired {};			// Event handler for unit fired
+			class eh_hit {};			// Event handler for unit hit
+			class eh_disconnected {};	// Event handler for unit disconnected
+			class eh_connected {};		// Event handler for unit connected
 		};
+	};
+};
+
+// XEH event handlers
+class Extended_PreInit_EventHandlers {
+    class ocap_pre_init_CBASettings {
+        init = "call ocap_fnc_init_CBASettings";
+    };
+};
+
+// OCAP Eden Editor module category
+class CfgFactionClasses
+{
+	class NO_CATEGORY;
+	class ocap_category: NO_CATEGORY
+	{
+		displayName = "OCAP";
+	};
+};
+
+// Eden editor modules
+class CfgVehicles
+{
+	class Logic;
+	class Module_F: Logic
+	{
+		class AttributesBase
+		{
+			class Default;
+			class Edit;					// Default edit box (i.e., text input field)
+			class Combo;				// Default combo box (i.e., drop-down menu)
+			class Checkbox;				// Default checkbox (returned value is Bool)
+			class CheckboxNumber;		// Default checkbox (returned value is Number)
+		};
+	};
+
+	// OCAP initalization module
+	class OCAP_ModuleInit: Module_F
+	{
+		// Standard module definitions
+		scope = 2; 						// Editor visibility; 2 will show it in the menu, 1 will hide it.
+		displayName = "OCAP Init"; 		// Name displayed in the menu
+		category = "ocap_category";		// Editor category ("NO_CATEGORY = "Misc")
+		function = "ocap_fnc_init";		// Name of function triggered once conditions are met
+		functionPriority = 1;			// Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
+		isGlobal = 0;					// 0 for server only execution, 1 for global execution, 2 for persistent global execution
+		isTriggerActivated = 1;			// 1 for module waiting until all synced triggers are activated
+		isDisposable = 1;				// 1 if modules is to be disabled once it's activated (i.e., repeated trigger activation won't work)
+		is3DEN = 0;						// 1 to run init function in Eden Editor as well
+
+		// Module user configurable settings
+		class Attributes: AttributesBase
+		{
+			class ExportPath: Edit
+			{
+				property = "ocap_ModuleInit_ExportPath";
+				displayName = "Export Filepath";
+				tooltip = "This is where all mission tracks will be saved to. If left blank the value saved in the CBA settings will be used.";
+				typeName = "STRING";
+			};
+			class FrameDelay: Edit
+			{
+				property = "ocap_ModuleInit_FrameDelay";
+				displayName = "Frame Delay";
+				tooltip = "Time in seconds to wait between each measurement. Lower = higher fidelity playback.";
+				typeName = "NUMBER";
+				defaultValue = "1";
+			};
+			class EndCaptureOnNoPlayers: Checkbox
+			{
+				property = "ocap_ModuleInit_EndCaptureOnNoPlayers";
+				displayName = "End Capture When No Players";
+				tooltip = "Automatically stop capture and export data if all players leave the server.";
+				typeName = "BOOL";
+				defaultValue = "false";
+			};
+			class EndCaptureOnEndMission: Checkbox
+			{
+				property = "ocap_ModuleInit_EndCaptureOnEndMission";
+				displayName = "End Capture When Mission Ends";
+				tooltip = "Automatically stop capture and export data if the mission ends.";
+				typeName = "BOOL";
+				defaultValue = "true";
+			};
+			class DebugMode: Checkbox
+			{
+				property = "ocap_ModuleInit_DebugMode";
+				displayName = "Debug Mode";
+				tooltip = "Enable debug mode. Generates verbose messages for debugging.";
+				typeName = "BOOL";
+				defaultValue = "false";
+			};
+		};
+	};
+
+	// OCAP export data module
+	class OCAP_ModuleExport: Module_F
+	{
+		// Standard module definitions
+		scope = 2; 						   // Editor visibility; 2 will show it in the menu, 1 will hide it.
+		displayName = "OCAP Export"; 	   // Name displayed in the menu
+		category = "ocap_category";		   // Editor category ("NO_CATEGORY = "Misc")
+		function = "ocap_fnc_exportData";  // Name of function triggered once conditions are met
+		functionPriority = 1;			   // Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
+		isGlobal = 0;					   // 0 for server only execution, 1 for global execution, 2 for persistent global execution
+		isTriggerActivated = 1;			   // 1 for module waiting until all synced triggers are activated
+		isDisposable = 0;				   // 1 if modules is to be disabled once it's activated (i.e., repeated trigger activation won't work)
+		is3DEN = 0;						   // 1 to run init function in Eden Editor as well
 	};
 };
--- @ocap/addons/ocap/functions/fn_init.sqf
+++ @ocap/addons/ocap/functions/fn_init.sqf
@@ -4,70 +4,95 @@
 	Description:
 	Initialises OCAP variables and mission-wide eventhandlers.
 	Capture loop is automatically started once init complete.
+	
+	Paramaters:
+	_logic = [OBJECT] Module settings
+	_units = [LIST] List of affected units
+	_activated = [BOOL] Whether or not the module is activated
 */
 
-// Define global vars
-#include "\userconfig\ocap\config.hpp";
-ocap_entitiesData = [];  // Data on all units + vehicles that appear throughout the mission.
-ocap_eventsData = []; // Data on all events (involving 2+ units) that occur throughout the mission.
-ocap_captureFrameNo = 0; // Frame number for current capture
-ocap_endFrameNo = 0; // Frame number at end of mission
+// Get Module settings
+_this params ["_logic", "_units", "_activated"];
 
-// Add mission EHs
-addMissionEventHandler ["EntityKilled", {
-	_victim = _this select 0;
-	_killer = _this select 1;
+if(_activated and isServer) then {
 
-	// Check entity is initiliased with OCAP
-	// TODO: Set ocap_exclude to true if unit is not going to respawn (e.g. AI)
-	if (_victim getVariable ["ocap_isInitialised", false]) then {
-		[_victim, _killer] call ocap_fnc_eh_killed;
+	// Define global vars
+	ocap_capture = true;
+	ocap_exportPath = _logic getVariable "ExportPath";
+	ocap_frameCaptureDelay = _logic getVariable "FrameDelay";
+	ocap_minPlayerCount = 0;
+	ocap_endCaptureOnNoPlayers = _logic getVariable "EndCaptureOnNoPlayers";
+	ocap_endCaptureOnEndMission = _logic getVariable "EndCaptureOnEndMission";
+	ocap_debug = _logic getVariable "DebugMode";
+	ocap_entitiesData = [];  // Data on all units + vehicles that appear throughout the mission.
+	ocap_eventsData = [];    // Data on all events (involving 2+ units) that occur throughout the mission.
+	ocap_captureFrameNo = 0; // Frame number for current capture
+	ocap_endFrameNo = 0;     // Frame number at end of mission
 
-		{
-			_victim removeEventHandler _x;
-		} forEach (_victim getVariable "ocap_eventHandlers");
-	};
-}];
+	// Set CBA setting export path if module path is empty
+	if (ocap_exportPath == "") then {
+		ocap_exportPath = ocap_ModuleInit_ExportPath_default;
+		};
+	
+	// Add mission EHs
+	addMissionEventHandler ["EntityKilled", {
+		_victim = _this select 0;
+		_killer = _this select 1;
 
-// Transfer ID from old unit to new unit
-// Mark old body to now be excluded from capture
-addMissionEventHandler ["EntityRespawned", {
-	_newEntity = _this select 0;
-	_oldEntity = _this select 1;
+		// Check entity is initiliased with OCAP
+		// TODO: Set ocap_exclude to true if unit is not going to respawn (e.g. AI)
+		if (_victim getVariable ["ocap_isInitialised", false]) then {
+			[_victim, _killer] call ocap_fnc_eh_killed;
 
-	if (_oldEntity getVariable ["ocap_isInitialised", false]) then {
-		_newEntity setVariable ["ocap_isInitialised", true];
-		_id = _oldEntity getVariable "ocap_id";
-		_newEntity setVariable ["ocap_id", _id];
-		_newEntity setVariable ["ocap_exclude", false];
-		_oldEntity setVariable ["ocap_exclude", true]; // Exclude old body from capture
-
-		_newEntity call ocap_fnc_addEventHandlers;
-	};
-}];
-
-addMissionEventHandler["HandleDisconnect", {
-	_unit = _this select 0;
-	_name = _this select 3;
-
-	if (_unit getVariable ["ocap_isInitialised", false]) then {
-		_unit setVariable ["ocap_exclude", true];
-	};
-
-	_name call ocap_fnc_eh_disconnected;
-}];
-
-addMissionEventHandler["PlayerConnected", {
-	_name = _this select 2;
-
-	_name call ocap_fnc_eh_connected;
-}];
-
-if (ocap_endCaptureOnEndMission) then {
-	addMissionEventHandler ["Ended", {
-		["Mission ended."] call ocap_fnc_log;
-		[] call ocap_fnc_exportData;
+			{
+				_victim removeEventHandler _x;
+			} forEach (_victim getVariable "ocap_eventHandlers");
+		};
 	}];
+
+	// Transfer ID from old unit to new unit
+	// Mark old body to now be excluded from capture
+	addMissionEventHandler ["EntityRespawned", {
+		_newEntity = _this select 0;
+		_oldEntity = _this select 1;
+
+		if (_oldEntity getVariable ["ocap_isInitialised", false]) then {
+			_newEntity setVariable ["ocap_isInitialised", true];
+			_id = _oldEntity getVariable "ocap_id";
+			_newEntity setVariable ["ocap_id", _id];
+			_newEntity setVariable ["ocap_exclude", false];
+			_oldEntity setVariable ["ocap_exclude", true]; // Exclude old body from capture
+
+			_newEntity call ocap_fnc_addEventHandlers;
+		};
+	}];
+
+	addMissionEventHandler["HandleDisconnect", {
+		_unit = _this select 0;
+		_name = _this select 3;
+
+		if (_unit getVariable ["ocap_isInitialised", false]) then {
+			_unit setVariable ["ocap_exclude", true];
+		};
+
+		_name call ocap_fnc_eh_disconnected;
+	}];
+
+	addMissionEventHandler["PlayerConnected", {
+		_name = _this select 2;
+
+		_name call ocap_fnc_eh_connected;
+	}];
+
+	if (ocap_endCaptureOnEndMission) then {
+		addMissionEventHandler ["Ended", {
+			["Mission ended."] call ocap_fnc_log;
+			[] call ocap_fnc_exportData;
+		}];
+	};
+
+	[] spawn ocap_fnc_startCaptureLoop;
 };
 
-[] spawn ocap_fnc_startCaptureLoop;
+// Dummy function return
+true
